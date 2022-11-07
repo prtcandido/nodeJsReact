be2386c90a0a8f1dbe316b7f9202fad3f240befc
Paulo Roberto Tazinazo Cândido <31222308+prtcandido@users.noreply.github.com>
2022-11-07 19:26:48 -0300

    Inicial

---
+++ servidor/src/routes.ts
@@ -0,0 +1,27 @@
+// Importa componentes do express
+import {Router} from 'express';
+// Importa Controllers
+import TesteController from './controllers/TesteController';
+import ProdutoController from './controllers/ProdutoController';
+// Validação dos parâmetos da rota
+import ValidaTeste1 from './middlewares/ValidaTeste1';
+// Instancia roteador
+const Roteador = Router();
+// Define rota tipo get que, para funcionar, deve ser requisitada conforme exemplo.
+// Exemplo de requisição: localhost:4000/teste/123?num=456
+// Onde 123 e 456 podem ser substituídos por quaisquer valores
+Roteador.get(
+    // URL com parêmetro :id
+    '/teste/:id',
+    ValidaTeste1,
+    // Aciona função do TesteController
+    new TesteController().teste1
+);
+
+Roteador.get('/produtos', new ProdutoController().index);
+Roteador.get('/produtos/:id', new ProdutoController().show);
+Roteador.post('/produtos', new ProdutoController().store);
+Roteador.put('/produtos/:id', new ProdutoController().update);
+Roteador.delete('/produtos/:id', new ProdutoController().delete);
+
+export default Roteador;
---
+++ servidor/src/server.ts
@@ -0,0 +1,12 @@
+// importa o express
+import express from 'express';
+// importar rotas
+import Roteador from './routes';
+// instancia o express
+const app = express();
+// Configurar uso de json
+app.use(express.json());
+// Configuração de uso das rotas
+app.use(Roteador);
+//configura porta e funçao executada na ativação
+app.listen(4000, ()=>{console.log("Servidor Iniciado")} );
